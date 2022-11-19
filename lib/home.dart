2243b960fc546e39ac32bff4b0def70c6958ec62
mahmoudgamal1704 <mahmoudgamal1704@gmail.com>
2022-11-19 20:22:58 +0200

    add wuran dart file and enable vcs

---
+++ lib/home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class HomeScreen extends StatelessWidget {
+  // const Home({Key? key}) : super(key: key);
+  static const RouteName='Home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Islamic'),
+      ),
+    );
+  }
+}
