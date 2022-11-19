8e99a2f1a1635450ff80082200a98900dbefd883
mahmoudgamal1704 <mahmoudgamal1704@gmail.com>
2022-11-19 20:24:08 +0200

    change home screen name

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Islamic'),
+        title: Text('Islami'),
       ),
     );
   }
